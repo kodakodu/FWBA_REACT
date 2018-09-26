7d239913c341db9b9748f76ea4330af84d814750
kodakodu <kodakodu@gmail.com>
2018-09-27 00:26:12 +0200

    Ajout 404

--- src/components/Kitacc.js
+++ src/components/Kitacc.js
@@ -5,6 +5,7 @@ import { withStyles } from 'material-ui/styles';
 import Drawer from 'material-ui/Drawer';
 import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
+import Grid from 'material-ui/Grid';
 import List from 'material-ui/List';
 import { MenuItem } from 'material-ui/Menu';
 import TextField from 'material-ui/TextField';
@@ -18,14 +19,21 @@ class Kitacc extends React.Component{
         return(
             <Layout>
                 {/* <h2>Kit Accueil</h2> */}
-                        <Typography variant="headline" gutterBottom>
-                        Pour mieux connaitre la MNT...
-                        </Typography>
-                <Typography variant="Headline">
-                Vous êtes nouvellement arrivé à la MNT et vous avez été identifié comme Partie Prenante à un projet...
-
-Pour vous aider à la découvrir, nous vous invitons à visualiser les documents suivants:
-                </Typography>
+                <Grid container spacing={12}>
+                    <Grid item xs={2}></Grid>
+                    <Grid item xs={8}>
+                            <Typography variant="headline" gutterBottom style={{textAlign:'center'}}>
+                            Pour mieux connaitre la MNT...
+                            </Typography>
+                            <Typography variant="Headline" gutterBottom style={{textAlign:'center'}}>
+                                Vous êtes nouvellement arrivé à la MNT et vous avez été identifié comme Partie Prenante à un projet...
+                            </Typography>
+                            <Typography variant="Headline" gutterBottom style={{textAlign:'center'}}>    
+                                Pour vous aider à la découvrir, nous vous invitons à visualiser les documents suivants:
+                            </Typography>
+                    </Grid>
+                    <Grid item xs={2}></Grid>
+                </Grid>
             </Layout>
         );
     }
--- src/components/notFound.js
+++ src/components/notFound.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import Typography from 'material-ui/Typography';
+import Grid from 'material-ui/Grid';
 import Layout from './Layout'
 
 const NotFound = () =>
 <Layout>
 {/* <h2>Kit Accueil</h2> */}
-        <Typography variant="headline" gutterBottom>
-        Erreur 404
-        </Typography>
-<Typography variant="Headline">
-Page introuvable !
-</Typography>
+        <Grid container spacing={12}>
+                <Grid item xs={2}></Grid>
+                <Grid item xs={8}>
+                        {/* <Typography variant="headline" gutterBottom style={{textAlign:'center'}}>
+                        Erreur 404...
+                        </Typography> */}
+                        <img src='img/404-page.png'></img>                           
+                </Grid>
+                <Grid item xs={2}></Grid>
+        </Grid>
 </Layout>
 
 export default NotFound;
